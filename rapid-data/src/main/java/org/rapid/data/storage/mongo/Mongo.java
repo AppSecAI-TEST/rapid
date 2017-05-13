a1f676c289db8b7e343a756ffbdce9794d826820
ahab-notebook <723854867@qq.com>
2017-05-13 10:16:38 +0800

    ahab

--- rapid-data/src/main/java/org/rapid/data/storage/mongo/Mongo.java
+++ rapid-data/src/main/java/org/rapid/data/storage/mongo/Mongo.java
@@ -1,11 +1,16 @@
 package org.rapid.data.storage.mongo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bson.Document;
 import org.bson.conversions.Bson;
+import org.rapid.util.common.serializer.SerializeUtil;
 
 import com.mongodb.MongoClient;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.model.FindOneAndUpdateOptions;
 
@@ -16,8 +21,7 @@ public class Mongo {
 	private MongoClient mongo;
 	private MongoDatabase connection;
 	
-	@SuppressWarnings("unused")
-	private void init() {
+	public void init() {
 		this.mongo = new MongoClient(host);
 		this.connection = mongo.getDatabase(db);
 	}
@@ -25,6 +29,26 @@ public class Mongo {
 	public void insertOne(String collectionName, Document document) {
 		MongoCollection<Document> collection = connection.getCollection(collectionName);
 		collection.insertOne(document);
+	}
+	
+	public <T> List<T> find(String collectionName, Class<T> clazz) { 
+		MongoCollection<Document> collection = connection.getCollection(collectionName);
+		FindIterable<Document> iterable = collection.find();
+		List<T> list = new ArrayList<T>(4);
+		MongoCursor<Document> cursor = iterable.iterator();
+		while (cursor.hasNext()) 
+			list.add(SerializeUtil.JsonUtil.GSON.fromJson(cursor.next().toJson(), clazz));
+		return list;
+	}
+	
+	public <T> List<T> find(String collectionName, Bson filter, Class<T> clazz) { 
+		MongoCollection<Document> collection = connection.getCollection(collectionName);
+		FindIterable<Document> iterable = collection.find(filter);
+		List<T> list = new ArrayList<T>(4);
+		MongoCursor<Document> cursor = iterable.iterator();
+		while (cursor.hasNext()) 
+			list.add(SerializeUtil.JsonUtil.GSON.fromJson(cursor.next().toJson(), clazz));
+		return list;
 	}
 	
 	public Document findOne(String collectionName, Bson filter) { 
